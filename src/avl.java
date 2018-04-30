8638c5254390e086dd41c1fc200e2b21057a1440
XeroxBoy <827312773@qq.com>
2018-04-30 16:16:23 +0800

    加了点内容 以及笔试时遇到的题

---
+++ src/avl.java
@@ -0,0 +1,35 @@
+public class avl {
+    static class Node{
+        Node left;
+        Node right;
+        int height;
+        int data;
+    }
+    Node tree;
+   int height(Node node){return node.height;}
+    public static Node rightR(Node a){
+        Node b=a.left;
+        a.left=b.right;
+        b.right=a;
+        a.height=a.left.height>a.right.height?a.left.height:a.right.height;
+        b.height=b.left.height>b.right.height?b.left.height:b.right.height;
+        return b;
+    }
+    public static Node leftR(Node a){
+       Node b=a.right;
+        a.right=b.left;
+        b.left=a;
+        a.height=a.left.height>a.right.height?a.left.height:a.right.height;
+        b.height=b.left.height>b.right.height?b.left.height:b.right.height;
+        return b;
+    }
+    public static Node leftRightR(Node a){
+        a.left=leftR(a.left);
+        return rightR(a);
+    }
+    public static Node rightLeftR(Node a){
+        a.right=rightR(a.right);
+        return leftR(a);
+    }
+}
+
