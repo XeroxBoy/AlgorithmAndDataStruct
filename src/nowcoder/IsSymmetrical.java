e4bff2ad661995aefb04d9201842140df3154e92
XeroxBoy <sb827312773@outlook.com>
2019-08-20 20:46:09 +0800

    practice

--- src/nowcoder/IsSymmetrical.java
+++ src/nowcoder/IsSymmetrical.java
@@ -5,65 +5,20 @@ import java.util.List;
 import java.util.Set;
 
 public class IsSymmetrical {
-    private List<TreeNode> leftList = new ArrayList<TreeNode>();
-    private List<Integer> leftDepthList = new ArrayList<Integer>();
-    private List<Integer> rightDepthList = new ArrayList<Integer>();
-    private List<TreeNode> rightList = new ArrayList<TreeNode>();
-
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        TreeNode left1 = new TreeNode(2);
-        TreeNode right1 = new TreeNode(2);
-        root.left = left1;
-        root.right = right1;
-        System.out.println(new IsSymmetrical().isSymmetrical(root));
-    }
-
     boolean isSymmetrical(TreeNode pRoot) {
-        middleLeftLoop(pRoot, pRoot, 1);
-        middleRightLoop(pRoot, pRoot, 1);
-        if (leftList.size() != rightList.size() || leftDepthList.size() != rightDepthList.size()) return false;
-        for (int i = 0; i < leftList.size(); i++) {
-            if (leftList.get(i).val != rightList.get(i).val) return false;
-            if (!leftDepthList.get(i).equals(rightDepthList.get(i))) return false;
+        if (pRoot == null) {
+            return true;
         }
-        return true;
+        return comRoot(pRoot.left, pRoot.right);
     }
 
-    void middleLeftLoop(TreeNode pRootLoop, TreeNode pRoot, int depth) {
-        if (pRootLoop == null) return;
-        if (pRootLoop.left != null) {
-            leftList.add(pRootLoop.left);
-            leftDepthList.add(depth);
-        }
-        middleLeftLoop(pRootLoop.left, pRoot, depth + 1);
-        if (pRootLoop == pRoot) {
-            return;
-        }
-        if (pRootLoop.right != null) {
-            leftList.add(pRootLoop.right);
-            leftDepthList.add(depth);
-        }
-        middleLeftLoop(pRootLoop.right, pRoot, depth + 1);
+    private boolean comRoot(TreeNode left, TreeNode right) {
+        // TODO Auto-generated method stub
+        if (left == null) return right == null;
+        if (right == null) return false;
+        if (left.val != right.val) return false;
+        return comRoot(left.right, right.left) && comRoot(left.left, right.right);
     }
-
-    void middleRightLoop(TreeNode pRootLoop, TreeNode pRoot, int depth) {
-        if (pRootLoop == null) return;
-        if (pRootLoop.right != null) {
-            rightList.add(pRootLoop.right);
-            rightDepthList.add(depth);
-        }
-        middleRightLoop(pRootLoop.right, pRoot, depth + 1);
-        if (pRootLoop == pRoot) {
-            return;
-        }
-        if (pRootLoop.left != null) {
-            rightList.add(pRootLoop.left);
-            rightDepthList.add(depth);
-        }
-        middleRightLoop(pRootLoop.left, pRoot, depth + 1);
-    }
-
     static class TreeNode {
         int val = 0;
         TreeNode left = null;
