f23e2fc4eabb489e6d4868c0975e6cb7fceb986a
XeroxBoy <sb827312773@outlook.com>
2019-10-04 22:33:57 +0800

    LeetCode easy

---
+++ src/LeetCode/Easy/Generate.java
@@ -0,0 +1,38 @@
+package LeetCode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate {
+    public static void main(String[] args) {
+        List<List<Integer>> generate = new Generate().generate(1);
+        for (int i = 0; i < generate.size(); i++) {
+            List<Integer> row = generate.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                System.out.print(row.get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows < 1) return new ArrayList<>();
+        List<List<Integer>> resultList = new ArrayList<>();
+        List<Integer> rowList = new ArrayList<>(1);
+        rowList.add(0, 1);
+        resultList.add(rowList);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> lastRowList = resultList.get(i - 1);
+            List<Integer> eachRowList = new ArrayList<>(i + 1);
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) {
+                    eachRowList.add(j, 1);
+                    continue;
+                }
+                eachRowList.add(j, lastRowList.get(j - 1) + lastRowList.get(j));
+            }
+            resultList.add(i, eachRowList);
+        }
+        return resultList;
+    }
+}
---
+++ src/LeetCode/Easy/HasPathSum.java
@@ -0,0 +1,30 @@
+package LeetCode.Easy;
+
+import dataStructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HasPathSum {
+    List<Integer> sumList = new ArrayList();
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        pathSum(root, root.val);
+        for (int i = 0; i < sumList.size(); i++) {
+            if (sumList.get(i).equals(sum)) return true;
+        }
+        return false;
+    }
+
+    public void pathSum(TreeNode root, int nowSum) {
+        if (root.left == null && root.right == null) {
+            sumList.add(nowSum);
+            return;
+        }
+        if (root.left != null)
+            pathSum(root.left, nowSum + root.left.val);
+        if (root.right != null)
+            pathSum(root.right, nowSum + root.right.val);
+    }
+}
