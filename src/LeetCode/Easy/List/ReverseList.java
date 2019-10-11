7b34a8485b2e0a71447640e78adf76e6d4ff4570
sx <827312773@qq.com>
2019-10-11 21:45:10 +0800

    leetcode easy

--- src/LeetCode/Easy/List/ReverseList.java
+++ src/LeetCode/Easy/List/ReverseList.java
@@ -2,6 +2,7 @@ package LeetCode.Easy.List;
 
 import dataStructure.ListNode;
 
+import java.util.List;
 import java.util.Stack;
 
 public class ReverseList {
@@ -11,27 +12,41 @@ public class ReverseList {
         ListNode node2 = new ListNode(3);
         head.next = node1;
         node1.next = node2;
-        ListNode newHead = new ReverseList().reverseList(head);
+//        ListNode newHead = new ReverseList().reverseList(null);
+        ListNode newHead = new ReverseList().reverseList2(head);
         while (newHead != null) {
-//            System.out.println(newHead.val);
+            System.out.println(newHead.val);
             newHead = newHead.next;
         }
     }
 
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList1(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
         while (head != null) {
             stack.push(head);
             head = head.next;
         }
+        if (stack.isEmpty()) return null;
         ListNode newHead = stack.pop();
         ListNode node = newHead;
-        System.out.println(node.val);
+//        System.out.println(node.val);
         while (!stack.isEmpty()) {
             ListNode nextNode = stack.pop();
             node.next = nextNode;
             node = nextNode;
         }
+        node.next = null;
         return newHead;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
