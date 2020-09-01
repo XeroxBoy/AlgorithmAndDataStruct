f10830c2966b90a3320af02d3f22dafe83de432c
XeroxBoy <sb827312773@outlook.com>
2020-09-01 22:59:33 +0800

    leetcode 每日打卡

---
+++ src/LeetCode/Medium/Array/PredictTheWinner.java
@@ -0,0 +1,21 @@
+package LeetCode.Medium.Array;
+
+public class PredictTheWinner {
+    public static void main(String[] args) {
+        new PredictTheWinner().PredictTheWinner(new int[]{1, 5, 233, 7});
+    }
+
+    public boolean PredictTheWinner(int[] nums) {
+        int length = nums.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] >= 0;
+    }
+}
